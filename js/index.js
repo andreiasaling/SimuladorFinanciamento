3db8fc9d365f68516cb9bc67f9b84b0a9f2a1a7f
andreiasaling <andreiasamira@gmail.com>
2022-07-10 10:04:56 -0300

    adiciona botão Limpar e ajusta CSS

--- js/index.js
+++ js/index.js
@@ -5,27 +5,28 @@ const comCarencia = document.querySelector('#comCarencia');
 const listaSuspensa = document.querySelector('#listaSuspensa');
 const corpoTabela = document.querySelector('#corpoTabela');
 const botaoCalcular = document.querySelector('#botaoCalcular');
+const botaoLimpar = document.querySelector('#botaoLimpar');
 const textoValor = document.querySelector('#textoValor');
 const textoEntrada = document.querySelector('#textoEntrada');
 const textoTaxaJuros = document.querySelector('#textoTaxaJuros');
 const textoPrazo = document.querySelector('#textoPrazo');
 
-function limpaCorpoTabela () {
-    while(corpoTabela.firstChild) {
+function limpaCorpoTabela() {
+    while (corpoTabela.firstChild) {
         corpoTabela.removeChild(corpoTabela.firstChild);
     }
 }
 
 //exibe-oculta lista suspensa com meses de carência
-comCarencia.addEventListener('change', function() {
+comCarencia.addEventListener('change', function () {
     if (this.checked) {
         listaSuspensa.removeAttribute('hidden');
     } else {
-        listaSuspensa.setAttribute('hidden','hidden');
+        listaSuspensa.setAttribute('hidden', 'hidden');
     }
 });
 
-botaoCalcular.addEventListener('click',function () {
+botaoCalcular.addEventListener('click', function () {
     limpaCorpoTabela();
     const valor = parseFloat(textoValor.value);
     const entrada = parseFloat(textoEntrada.value);
@@ -34,11 +35,21 @@ botaoCalcular.addEventListener('click',function () {
     let simulacao;
     if (comCarencia.checked) {
         const carencia = parseInt(listaSuspensa.value);
-        simulacao = new FinanciamentoCarencia(valor,entrada,taxaJuros,prazo,carencia);
+        simulacao = new FinanciamentoCarencia(valor, entrada, taxaJuros, prazo, carencia);
     } else {
-        simulacao = new Financiamento(valor,entrada,taxaJuros,prazo);
+        simulacao = new Financiamento(valor, entrada, taxaJuros, prazo);
     }
-    // simulacao = new Financiamento(valor,entrada,taxaJuros,prazo);
     simulacao.calcParcelasMensais();
     simulacao.exibeParcelas();
+});
+
+botaoLimpar.addEventListener('click', function () {
+    textoValor.value = '';
+    textoEntrada.value = '';
+    textoTaxaJuros.value = '';
+    textoPrazo.value = '';
+    comCarencia.checked = false;
+    listaSuspensa.setAttribute('hidden','hidden');
+    limpaCorpoTabela();
+    textoValor.focus();
 });
